a1be802a004959f0f8f71ade0a9a426ff951ec2b
sasamafi5-cloud <sasamafi5@googlemail.com>
2026-05-24 14:56:59 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'grocery-ai-v1';
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
